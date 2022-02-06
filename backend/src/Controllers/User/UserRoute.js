450ea7d4814ee0727e2402afd4732a5df439fcc4
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2022-02-05 22:23:49 -0300

    check account

--- backend/src/Controllers/User/UserRoute.js
+++ backend/src/Controllers/User/UserRoute.js
@@ -1,28 +1,51 @@
 const bcrypt = require('bcrypt');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 require('dotenv/config');
 
 const router = express.Router();
 
 const User = require('../../Models/User');
+const CheckField = require('../../Utils/Validation');
 
 router.use((req, res, next) => {
     next();
 });
 
+router.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+
+    if(CheckField(username) && CheckField(password)) {
+
+        const account = await User.findOne( { where: {username} } );
+        let checkPassword = bcrypt.compareSync(password, account.password);
+        
+        if(account != null || checkPassword) {
+            res.status(200).json({msg: 'ok'});
+        }else {
+            res.status(404).json({msg: 'Usuário não existe!'});
+        }
+        
+    }else {
+        res.status(400).json({msg: 'Campos não podem ser nulos!'});
+    }
+
+});
+
 router.post('/register', async (req, res) => {
 
-    const {name, password} = req.body;
+    const {name, username, password} = req.body;
 
     await User.create({
         name,
+        username,
         password: bcrypt.hashSync(password, 10),
         permission: 'USER'
     }).then((response) => {
-        res.status(201).json({msg: 'O usuário foi criado com sucesso!'})
+        res.status(201).json({msg: 'O usuário foi criado com sucesso!'});
+    }).catch(err => {
+        res.status(422).json({msg: 'Não foi possível criar usuário.', err});
     });
-
-    res.status(422).json({msg: 'Não foi possível criar usuário.'});
     
 });
 
--- backend/src/Models/User.js
+++ backend/src/Models/User.js
@@ -2,8 +2,9 @@ const {DataTypes} = require('sequelize');
 const db = require('../db/conn');
 
 const User = db.define('User', {
-    name: {type: DataTypes.STRING, required: true},
-    password: {type: DataTypes.STRING, required: true},
+    name: {type: DataTypes.STRING, allowNull:false, required: true},
+    username: {type: DataTypes.STRING, allowNull:false, required: true, unique: true},
+    password: {type: DataTypes.STRING, allowNull:false, required: true},
     permissions: {type: DataTypes.STRING, defaultValue: `USER`}
 });
 
